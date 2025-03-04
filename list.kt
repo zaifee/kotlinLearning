b70b5711874fb31e8a972fbfd7812e0c65766c82
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2025-03-04 23:51:04 +0530

    Create list.kt

---
+++ list.kt
@@ -0,0 +1,24 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+
+
+fun main() {
+    
+    val fruitList = mutableListOf("Apple", "Mango", "Banana");
+    
+    fruitList.add("Grapes")
+//     fruitList.remove("Mango")
+
+
+	println(fruitList.size)    
+    
+//     for(item in 0 until < fruitList.size){
+        
+//         if(item == "Banana"){
+//             fruitList.removeLast()
+//         }
+        
+//     }
+}
